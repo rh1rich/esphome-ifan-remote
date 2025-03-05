363cadbb85fe0c04874564ee1d42f70fd5f21fcc
rh1rich <roland.heinrich@rh-net.net>
2025-03-05 20:27:11 +0100

    Code optimization

--- components/ifan_remote/ifan_remote.cpp
+++ components/ifan_remote/ifan_remote.cpp
@@ -23,7 +23,7 @@ bool IFanRemoteComponent::process_byte_(uint8_t byte) {
     return byte == CMD_START2;
   }
 
-  // Byte 1-6: Command
+  // Byte 2-6: Command
   if (pos < (CMD_SIZE - 1))
     return true;
 
@@ -33,35 +33,28 @@ bool IFanRemoteComponent::process_byte_(uint8_t byte) {
   command.high = (raw[2] << 8) | raw[3];
   command.low = (raw[4] << 16) | (raw[5] << 8) | raw[6];
   if (chksum == raw[7]) {
-    ESP_LOGI(TAG, "Received command: high=0x%04X low=0x%06X", command.high, command.low);
+    ESP_LOGD(TAG, "Received command %04X:%06X", command.high, command.low);
     this->data_callback_.call(command);
   }
   else {
-    ESP_LOGW(TAG, "Received command with invalid checksum: high=0x%04X low=0x%06X chksum=%02X", command.high, command.low, chksum);
+    ESP_LOGW(TAG, "Received command %04X:%06X with invalid checksum %02X. Should be %02X.", command.high, command.low, raw[7], chksum);
   }
-
-  ESP_LOGVV(TAG, "Processed byte: 0x%02X", byte);
 
   // return false to reset buffer
   return false;
 }
 
 void IFanRemoteComponent::loop() {
-  const uint32_t now = millis();
-  if (now - this->rx_last_millis_ > 50) {
-    this->rx_buffer_.clear();
-    this->rx_last_millis_ = now;
-  }
-
   while (this->available()) {
     uint8_t byte;
     this->read_byte(&byte);
-    if (this->process_byte_(byte)) {
-      ESP_LOGVV(TAG, "Processed byte: 0x%02X", byte);
-      this->rx_last_millis_ = now;
-    } else {
+    if (millis() - this->rx_last_millis_ > 50) {
       this->rx_buffer_.clear();
     }
+    if (!this->process_byte_(byte)) {
+      this->rx_buffer_.clear();
+    }
+    this->rx_last_millis_ = millis();
   }
 }
 
